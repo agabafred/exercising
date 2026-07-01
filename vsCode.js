70644a75fd0366379514d291e07b287d69094b60
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-01 22:40:07 +0200

    done to udjust

--- vsCode.js
+++ vsCode.js
@@ -12,3 +12,6 @@ else if(totalYearsOfIndependance>=36){
 else{
     console.log('there are many years past of celebrating independance years');
 }
+
+// this is a simple code to calculate the total years of independence in rwanda and also to check if the total years of independence is less than or equal to 30 or greater than or equal to 36 or in between 30 and 36
+let barinda = "my name is barinda and i am a software engineer";
