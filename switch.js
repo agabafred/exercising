9d66be7fa9a6a820b4b0de01aaddb40c48ed0906
agaba fred <agabafredparabure@gmail.com>
2026-07-02 15:16:43 -0700

    this is switch

---
+++ switch.js
@@ -0,0 +1,27 @@
+console.log('choose the correct day of independance in rwanda ');
+let day=6;
+switch (day){
+    case 1:
+        console.log('this is monday');
+        break;
+        case 2:
+            console.log('this is teusday');
+            break;
+            case 3:
+                console.log('this is wednesday');
+                break;
+                case 4:
+                console.log('this is thursday');
+                break;
+                case 5:
+                console.log('this is friday');
+                break;
+                case 6:
+                    console.log('this the real day of independance');
+                    break;
+                    case 7:
+                    console.log('this also sunday');
+                    break;
+                    default:
+                        console.log('this means you enterd the wrong day');
+                    }
