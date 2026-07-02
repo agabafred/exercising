ab0a64352ff14905e7f12baa908242b4ad32051b
agaba fred <agabafredparabure@gmail.com>
2026-07-02 15:54:21 -0700

    this is if else

---
+++ ifElse.js
@@ -0,0 +1,9 @@
+console.log('lets see if you are able to vote ');
+let name= 'agaba';
+console.log('if your agaba your allowed to vote and if your not wait for yous');
+if(name === 'agaba'){
+    console.log('your allowed to vote');
+}
+else{
+    console.log('your not allowed wait for your name ');
+}
