af447eeaa89128c01b483ed627cb5ee5953e40cc
agaba fred <agabafredparabure@gmail.com>
2026-07-01 22:11:25 -0700

    its well done

--- vsCode.js
+++ vsCode.js
@@ -3,3 +3,9 @@ let currentYearOfIndependance=(2026);
 let firstYearOfIndependence=(1990);
 let totalYearsOfIndependance=(currentYearOfIndependance-firstYearOfIndependence);
 console.log(`the total years of independance in rwanda is equal to : ${totalYearsOfIndependance}`);
+if (totalYearsOfIndependance<=30){
+    console.log('no many years past ');
+}
+else{
+    console.log('there are many years past of celebrating independance years');
+}
