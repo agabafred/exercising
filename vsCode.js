ff9d6b28ab2427ff56878ca16c79a3ed3ec0bf1a
agaba fred <agabafredparabure@gmail.com>
2026-07-01 21:56:37 -0700

    its well running

---
+++ vsCode.js
@@ -0,0 +1,5 @@
+console.log('hellow how is independence day my brother');
+let currentYearOfIndependance=(2026);
+let firstYearOfIndependence=(1990);
+let totalYearsOfIndependance=(currentYearOfIndependance-firstYearOfIndependence);
+console.log(`the total years of independance in rwanda is equal to : ${totalYearsOfIndependance}`);
