cf4cd84c82dd8a4391c4a5fb8be5c01c384ccf68
agaba fred <agabafredparabure@gmail.com>
2026-07-03 03:10:52 -0700

    this is java basics especially length,replace

---
+++ length.js
@@ -0,0 +1,9 @@
+console.log('lecture'.length);
+console.log('lecture'.toUpperCase());
+console.log('LECTURE'.toLowerCase());
+console.log('  yes   agaba  is student   '.trim().replaceAll(/\s/g,''));
+//trim removes white spaces on first and end of the texts only but not between texts
+console.log('lets replace my name of lectute into student :');
+/* i wanted to replace the name lecture and i placed it into 
+student by writting console.log('lecture'.replace('lecture','student'));*/
+console.log('lecture'.replace('lecture','student' + ' :its now done'));
