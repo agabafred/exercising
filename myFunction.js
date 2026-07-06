9d6552bea9fd9d7ab2cc90812d06159d60eacb4e
agaba fred <agabafredparabure@gmail.com>
2026-07-05 22:28:54 -0700

    function coded example

---
+++ myFunction.js
@@ -0,0 +1,13 @@
+ function great(name,block,years,number){
+     return `my name is ${ name} and my class im in class of ${ block} as im ${ years} years and i play as number${ number} in playground`
+     } 
+     function numbers(num1,num2){
+         return num1*num2
+     }
+console.log(great('agaba','level5',29,9));
+console.log(great('fred','level5',27,00));
+console.log(great('mugbo','level5',26,55));
+console.log(great('kagame','level5',24,56));
+console.log(great('brain','level5',20,51));
+console.log('the multiplication is ::');
+console.log(numbers(2,3));
