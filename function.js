055c2e2edcd4d4bc3e4169fff5b2464a85ac3364
agaba fred <agabafredparabure@gmail.com>
2026-07-07 00:13:00 -0700

    basic function testing

---
+++ function.js
@@ -0,0 +1,9 @@
+function userName(name,age){
+     console.log(`happy birth day dear ${name} `)
+     console.log(`grow wide as your ${age} years old`)
+     console.log('enjoy your day'.replace('enjoy your day','stay happy'));
+ }
+  userName('agaba',21);
+  userName('lidia',27);
+  userName('phina',29);
+  userName('dan',20);
