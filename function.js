7b5d63d9c699d7cd48f09dd2137e83491c3dab91
agaba fred <agabafredparabure@gmail.com>
2026-07-09 14:47:19 -0700

    function calculations

--- function.js
+++ function.js
@@ -1,9 +1,17 @@
-function userName(name,age){
-     console.log(`happy birth day dear ${name} `)
-     console.log(`grow wide as your ${age} years old`)
-     console.log('enjoy your day'.replace('enjoy your day','stay happy'));
+ function subtract(x,y){
+    return x-y;
  }
-  userName('agaba',21);
-  userName('lidia',27);
-  userName('phina',29);
-  userName('dan',20);
+
+ function add(x,y){
+     return x+y;
+ }
+ function multiply(x,y){
+     return x*y;
+ }
+ function divide(x,y){
+     return x/y;
+}
+ //console.log(subtract(3,2));
+  console.log('this is subtraction',subtract(3,2));
+   console.log('this is multiplication',multiply(3,2));
+    console.log('this is division',divide(3,2));
---
+++ myArray.js
@@ -0,0 +1,5 @@
+let array1=[9,8,7,6,5,4,3,2,1,'agaba',5.6]
+// sort array method that is responsible for sorting values in array
+console.log('this is sorted array :',array1.sort((a,b)=>a-b))
+// filter array method 
+
