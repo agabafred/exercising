88447939f4560a292f6912a1f5beeec7034f881d
agaba fred <agabafredparabure@gmail.com>
2026-07-10 15:48:23 -0700

    test loop basics

---
+++ loops.js
@@ -0,0 +1,39 @@
+ console.log('this is for loop statement');
+for (let i=0;i<4;i++){
+    console.log(i);
+}
+console.log('   ')
+console.log('while loop statement'); 
+let j=0;
+while (j<4){
+    console.log(j);
+    j++ ;
+}
+console.log('   ')
+console.log('this is do while loop');
+let z=0;
+do {
+    console.log(z)
+    z++;
+}while(z<4);
+
+console.log('   ');
+console.log('this is for...of loop');
+let t=['mango','amata'];
+for (let mama of t){
+    console.log(t)
+}
+console.log('   ')
+console.log('this is for...in loop');
+let d=['juice','water'];
+for(let papa in d);
+console.log(d)
+
+console.log('   ')
+console.log('this is for.Each loop ');
+['papa','mama','guys','ladies'].forEach(familyMembers => console.log(familyMembers));
+
+
+
+
+
