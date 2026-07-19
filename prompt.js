170925cdd6d1dbef2a690fa64807fbd2fbf704b3
agaba fred <agabafredparabure@gmail.com>
2026-07-18 17:31:05 -0700

    lets user to enter data

---
+++ prompt.js
@@ -0,0 +1,18 @@
+ //letting user eneter the valeu or argument wants and we convert it int nbers if its number from the string as its default
+  // we use e.x: 1)socialMarks=prompt ('enetr you marks') 2) let marks=Number(socialMarks)
+  let age= prompt ('enetr also your age broo : ');
+console.log('you entered the age of :' + age);
+console.log('lets add 2 :');
+let sum = age + 2 ;
+console.log('your sum is ' + age)
+
+console.log('    ')
+console.log('lets correct it ');
+
+let agee=prompt('enetr also your age broo : ');
+console.log('you entered the age of :' + age)
+
+let ages=Number(agee);
+console.log('lets add 2 :');
+let sums = ages + 2 ;
+console.log('your sum is ' + sums);
