43fbe1e20c82f58d1aeb8f65c61ac7070b971a0a
agaba fred <agabafredparabure@gmail.com>
2026-07-18 17:39:57 -0700

    simple for loop example

---
+++ forLoop.js
@@ -0,0 +1,6 @@
+  // THIS IS FOR LOOP 
+const vistingAreas=['kigali','kampala','bwera'];
+for (let i = 0 ; i < vistingAreas.length ; i ++){
+console.log(' i would like to visit ' + vistingAreas[i]);
+}
+console.log('the length is : ' + vistingAreas.length);
